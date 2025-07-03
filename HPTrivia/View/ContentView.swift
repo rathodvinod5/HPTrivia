2900669422f9cf9f16df9b6cf5cd9e80f695737a
rathodvinod5 <rathodvinod5@gmail.com>
2025-07-03 22:20:30 +0530

    added animation to title text

--- HPTrivia/View/ContentView.swift
+++ HPTrivia/View/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animateViewsIn = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -22,10 +24,34 @@ struct ContentView: View {
                             .linear(duration: 60)
                     }
 
+                VStack {
+                    VStack {
+                        if animateViewsIn {
+                            VStack {
+                                Image(systemName: "bolt.fill")
+                                    .imageScale(.large)
+                                    .font(.largeTitle)
+                                Text("HP")
+                                    .font(.custom("PartyLetPlain", size: 70))
+                                    .padding(.bottom, -50)
+                                Text("Trivia")
+                                    .font(.custom("PartyLetPlain", size: 60))
+                            }
+                            .padding(.top, 70)
+                            .transition(.move(edge: .top))
+                        }
+                    }
+                    .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
+                    
+                    Spacer()
+                }
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+                animateViewsIn = true
+        }
     }
 }
 
